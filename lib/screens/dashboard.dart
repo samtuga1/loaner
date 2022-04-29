8f8c193d164af85c20f6d329017b18b1846eb2a1
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-29 17:18:55 +0000

    huge updates

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:loaner/widgets/greetings_banner.dart';
+import '../widgets/balance_card.dart';
+import '../widgets/greetings_banner.dart';
+import '../widgets/loan_card.dart';
+import '../widgets/loan_type.dart';
 
 class Dashboard extends StatelessWidget {
   static const routeName = '/dashboard';
@@ -9,12 +12,55 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
+      drawer: const Drawer(),
       key: _scaffoldKey,
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: Column(
         children: [
           GreetingBarner(dashboardKey: _scaffoldKey),
+          const BalanceCard(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              LoanType(
+                title: 'Personal',
+                imgUrl: 'assets/images/personal-money.png',
+              ),
+              LoanType(
+                title: 'Home',
+                imgUrl: 'assets/images/house.png',
+              ),
+              LoanType(
+                title: 'EMI',
+                imgUrl: 'assets/images/calculator.png',
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 18.0, right: 20, top: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Recommended Loans',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                FlatButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'View all',
+                    style: TextStyle(fontSize: 15, color: Colors.black54),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (ctx, i) => const LoanCard(),
+            ),
+          )
         ],
       ),
     );

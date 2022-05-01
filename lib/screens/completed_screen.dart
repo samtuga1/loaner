29e7f6e9ed636ca184109a1f1fe243d060192405
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-05-01 23:53:42 +0000

    update

---
+++ lib/screens/completed_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CompletedScreen extends StatelessWidget {
+  const CompletedScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

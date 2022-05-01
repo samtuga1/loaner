f1db78a744a670d7a31a10f48c9da22df0165691
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-05-01 23:54:25 +0000

    added body

--- lib/screens/completed_screen.dart
+++ lib/screens/completed_screen.dart
@@ -5,6 +5,8 @@ class CompletedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(),
+    );
   }
 }

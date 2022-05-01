5215ebc13ac04df98981193deea4c59581cc3803
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-05-01 19:52:42 +0000

    Implemented my get loans screen

---
+++ lib/screens/get_loans_screen.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:loaner/widgets/stepper_buttons.dart';
+import '../screens/loan_detail_screen.dart';
+
+class GetLoansScreen extends StatefulWidget {
+  static const routeName = '/get_loans_screen';
+  const GetLoansScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GetLoansScreen> createState() => _GetLoansScreenState();
+}
+
+class _GetLoansScreenState extends State<GetLoansScreen> {
+  double maxAmount = 30;
+  int _currentStep = 0;
+  var _onStepContinue;
+  var _onStepCancel;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Theme.of(context).scaffoldBackgroundColor,
+          colorScheme: const ColorScheme.light(
+            primary: Colors.black,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(15),
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 8),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(width: 0.4),
+                        ),
+                        child: const Padding(
+                          padding:
+                              EdgeInsets.only(left: 8.0, top: 3, bottom: 3),
+                          child: Icon(Icons.arrow_back_ios),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 25,
+                    ),
+                    const Text(
+                      'Get Loan',
+                      style:
+                          TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Stack(
+                  children: [
+                    Stepper(
+                      controlsBuilder: (context, details) {
+                        _onStepContinue = details.onStepContinue;
+                        _onStepCancel = details.onStepCancel;
+                        return const SizedBox.shrink();
+                      },
+                      elevation: 0,
+                      type: StepperType.horizontal,
+                      steps: [
+                        loanDetailScreen(),
+                        Step(
+                          isActive: _currentStep >= 1,
+                          title: const Text('Bank Details'),
+                          content: Container(),
+                        ),
+                        Step(
+                          isActive: _currentStep >= 2,
+                          title: const Text('Get Loan'),
+                          content: Container(),
+                        ),
+                      ],
+                      currentStep: _currentStep,
+                      onStepTapped: (int newIndex) {
+                        setState(() {
+                          _currentStep = newIndex;
+                        });
+                      },
+                      onStepContinue: () {
+                        if (_currentStep != 2) {
+                          setState(() {
+                            _currentStep++;
+                          });
+                        }
+                      },
+                      onStepCancel: () {
+                        if (_currentStep != 0) {
+                          setState(() {
+                            _currentStep--;
+                          });
+                        }
+                      },
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: StepperButton(nextPressed: () {
+                        _onStepContinue();
+                      }, backPressed: () {
+                        _onStepCancel();
+                      }),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

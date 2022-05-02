3d6a8075ff87df91b554925d7165c083e17423b2
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-05-02 16:31:37 +0000

    update

--- lib/screens/recommended_loans_screen.dart
+++ lib/screens/recommended_loans_screen.dart
@@ -9,6 +9,7 @@ class RecommendedLoansScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final loans = Provider.of<loans_provider.Loans>(context, listen: false);
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -64,12 +65,25 @@ class RecommendedLoansScreen extends StatelessWidget {
                       return Consumer<loans_provider.Loans>(
                         builder: (context, loanData, _) => ListView.builder(
                           itemBuilder: (ctx, i) {
+                            final loanId = loanData.recommendedLoans[i].id;
+                            final type = loanData.recommendedLoans[i].loanType;
+                            final rate = loanData.recommendedLoans[i].rate;
+                            final maxAmount =
+                                loanData.recommendedLoans[i].maxAmount;
+                            final time = loanData.recommendedLoans[i].time;
+                            final emi = loans.calculateEMI(
+                                maxAmount: maxAmount, time: time, rate: rate);
+                            final totalToBePayed =
+                                loans.totalToRepay(emi: emi, time: time);
+                            final interest = loans.calculateInterest(
+                                maxAmount: maxAmount,
+                                totalToBePaid: totalToBePayed);
                             return LoanCard(
-                              loanType: loanData.recommendedLoans[i].loanType,
-                              maxAmount: loanData.recommendedLoans[i].maxAmount!
-                                  .toDouble(),
-                              interest: loanData.recommendedLoans[i].interest!
-                                  .toDouble(),
+                              id: loanId,
+                              loanType: type,
+                              maxAmount: maxAmount!.toDouble(),
+                              interest: interest,
+                              rate: rate,
                             );
                           },
                           itemCount: loanData.recommendedLoans.length,

2e1d5047f81841726066d8ffcacbe44f0677029f
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-05-04 13:32:33 +0000

    update

--- lib/screens/loan_detail_screen.dart
+++ lib/screens/loan_detail_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_sliders/sliders.dart';
-import 'package:syncfusion_flutter_core/theme.dart';
 import 'package:dotted_line/dotted_line.dart';
 
 Step loanDetailScreen(

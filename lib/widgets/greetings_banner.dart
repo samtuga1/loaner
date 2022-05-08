75329bc89e308b8fc6edf0e69c914d8a6e0ec5e8
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-05-08 16:15:40 +0000

    Implemented Major updates

--- lib/widgets/greetings_banner.dart
+++ lib/widgets/greetings_banner.dart
@@ -1,90 +1,125 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:loaner/providers/greeting_card.dart';
+import 'package:provider/provider.dart';
+import '../providers/user.dart';
 
 class GreetingBarner extends StatelessWidget {
   const GreetingBarner({Key? key, this.drawerKey}) : super(key: key);
   final dynamic drawerKey;
 
+  String getGreeting() {
+    int hour = DateTime.now().hour;
+    if (hour < 12) {
+      return 'Good morning!';
+    } else if (hour >= 12 && hour < 17) {
+      return 'Good afternoon!';
+    } else {
+      return 'Good evening!';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: FirebaseStorage.instance
-          .ref('user_images')
-          .child(FirebaseAuth.instance.currentUser!.uid + '.jpg')
-          .getDownloadURL(),
-      builder: (context, snapshot) => PhysicalModel(
-        elevation: 5,
-        borderRadius: const BorderRadius.only(
-          bottomLeft: Radius.circular(15),
-          bottomRight: Radius.circular(15),
-        ),
-        shadowColor: Colors.black54,
-        color: Colors.grey,
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          alignment: Alignment.topCenter,
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            color: Color(0xFFFFFFFF),
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(15),
-              bottomRight: Radius.circular(15),
-            ),
+    return PhysicalModel(
+      elevation: 5,
+      borderRadius: const BorderRadius.only(
+        bottomLeft: Radius.circular(15),
+        bottomRight: Radius.circular(15),
+      ),
+      shadowColor: Colors.black54,
+      color: Colors.grey,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        alignment: Alignment.topCenter,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: Color(0xFFFFFFFF),
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(15),
+            bottomRight: Radius.circular(15),
           ),
-          child: SafeArea(
-            child: Row(
-              children: [
-                IconButton(
-                  onPressed: () {
-                    drawerKey.currentState?.openDrawer();
-                  },
-                  icon: const Icon(Icons.menu),
-                ),
-                Expanded(
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 27,
-                      backgroundImage: NetworkImage(
-                        snapshot.connectionState == ConnectionState.waiting
-                            ? 'https://monstar-lab.com/global/wp-content/uploads/sites/11/2019/04/male-placeholder-image.jpeg'
-                            : snapshot.data.toString(),
-                      ),
+        ),
+        child: SafeArea(
+          child: Row(
+            children: [
+              Expanded(
+                child: ListTile(
+                  leading: const ProfilePhotoContainer(),
+                  title: const Username(),
+                  subtitle: Text(
+                    getGreeting(),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 23,
+                      color: Colors.black,
                     ),
-                    title: const Text(
-                      'Welcome Samuel!',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  ), //GetTime(),
+                  trailing: Container(
+                    width: 38,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1.2),
+                      shape: BoxShape.circle,
                     ),
-                    subtitle: const Text(
-                      'Good morning!',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 23,
+                    child: const Center(
+                      child: Icon(
+                        Icons.notifications_none_sharp,
                         color: Colors.black,
                       ),
                     ),
-                    trailing: Container(
-                      width: 38,
-                      decoration: BoxDecoration(
-                        border: Border.all(width: 1.2),
-                        shape: BoxShape.circle,
-                      ),
-                      child: const Center(
-                        child: Icon(
-                          Icons.notifications_none_sharp,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
                   ),
-                )
-              ],
-            ),
+                ),
+              )
+            ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfilePhotoContainer extends StatelessWidget {
+  const ProfilePhotoContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Provider.of<ProfilePicture>(context, listen: false)
+          .getProfilePicture(),
+      builder: (context, snapshot) => Consumer<ProfilePicture>(
+        builder: (context, photo, _) => CircleAvatar(
+          radius: 24,
+          backgroundImage: snapshot.connectionState == ConnectionState.waiting
+              ? const NetworkImage(
+                  'https://monstar-lab.com/global/wp-content/uploads/sites/11/2019/04/male-placeholder-image.jpeg',
+                )
+              : NetworkImage(photo.profilePicture!),
+        ),
+      ),
+    );
+  }
+}
+
+class Username extends StatelessWidget {
+  const Username({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final String? username =
+        Provider.of<Users>(context, listen: false).username;
+    return FutureBuilder(
+      future: Provider.of<Users>(context).getUsername(),
+      builder: (context, snapshot) => Text(
+        snapshot.connectionState == ConnectionState.waiting
+            ? 'Welcome !'
+            : 'Welcome $username!',
+        style: const TextStyle(
+          fontSize: 15,
+          color: Colors.grey,
+          fontWeight: FontWeight.bold,
         ),
       ),
     );

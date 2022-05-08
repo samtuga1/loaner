a50fca855e5fcb3e1140572dee85df71d105b329
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-05-08 16:09:09 +0000

    Added some couple of screens to the route table

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:loaner/providers/loan.dart';
-import 'package:loaner/providers/user_loan.dart';
-import 'package:loaner/screens/completed_screen.dart';
-import 'package:loaner/screens/dashboard.dart';
+import './providers/greeting_card.dart';
+import './providers/loan.dart';
+import './providers/user.dart';
+import './screens/add_loan_screen.dart';
+import './screens/completed_screen.dart';
+import './screens/dashboard.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:loaner/screens/get_loans_screen.dart';
-import 'package:loaner/screens/home_loans_screen.dart';
-import 'package:loaner/screens/my_loans_screen.dart';
-import 'package:loaner/screens/recommended_loans_screen.dart';
+import './screens/get_loans_screen.dart';
+import './screens/home_loans_screen.dart';
+import './screens/my_loans_screen.dart';
+import './screens/recommended_loans_screen.dart';
 import 'package:provider/provider.dart';
-import 'screens/auth_screen.dart';
-import 'screens/personal_loans_screen.dart';
+import 'providers/requested_loan.dart';
+import './screens/auth_screen.dart';
+import './screens/personal_loans_screen.dart';
+import './screens/requested_loans_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,7 +32,9 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
         providers: [
           ChangeNotifierProvider(create: (context) => Loans()),
-          ChangeNotifierProvider(create: (context) => UserLoans()),
+          ChangeNotifierProvider(create: (context) => RequestedLoans()),
+          ChangeNotifierProvider(create: (context) => ProfilePicture()),
+          ChangeNotifierProvider(create: (context) => Users()),
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false, //Removes the debug banner
@@ -56,7 +62,9 @@ class MyApp extends StatelessWidget {
             GetLoansScreen.routeName: (context) => const GetLoansScreen(),
             CompletedScreen.routeName: (context) => const CompletedScreen(),
             Dashboard.routeName: (context) => Dashboard(),
-            MyLoansScreen.routeName: (context) => MyLoansScreen(),
+            RequestedLoansScreen.routeName: (context) => RequestedLoansScreen(),
+            AddLoanScreen.routname: (context) => AddLoanScreen(),
+            MyLoansScreen.routeName: (context) => const MyLoansScreen(),
           },
         ));
   }
